f321d57fef0ab07fc4946a71ec6e26ea3540c7ec
kylesloper <kylesloper@gmail.com>
2025-07-22 20:01:36 +0100

    updated deps

--- deps.config.ts
+++ deps.config.ts
@@ -3,7 +3,7 @@ export default {
     jsr: [
         "@std/html@1.0.4",
         "@std/html@1.0.3", //  Have zero idea why this is needed, but it is
-        "@std/yaml@1.0.8",
+        "@std/yaml@1.0.9",
         "@std/front-matter@1.0.9",
         "@std/fs@1.0.19",
         "@std/path@1.1.1",
@@ -12,7 +12,7 @@ export default {
         "@std/encoding@1.0.10",
         "@std/media-types@1.1.0",
         "@std/log@0.224.14",
-        "@hono/hono@4.8.4",
+        "@hono/hono@4.8.5",
         "@davidbonnet/astring@1.8.6",
     ],
     npm: [
