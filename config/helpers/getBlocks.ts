fa1fbe3639db8306780e87b71c374fa7212e485f
kylesloper <kylesloper@gmail.com>
2025-08-10 18:21:52 +0100

    prod

--- config/helpers/getBlocks.ts
+++ config/helpers/getBlocks.ts
@@ -1,10 +1,5 @@
 // import * as blocks from "../../../../../../lume-cms-partials/blocks/index.js"
 import * as blocks from "https://esm.sh/gh/moonfacedigital/lume-cms-partials@45ebff739efb93cd0f4828ae1ce09a60aa5c7593/blocks/index.js"
-import { getStore } from "@netlify/blobs"
-import { type Context } from "https://edge.netlify.com/"
-
-const CACHE_KEY = "processed-blocks"
-const CACHE_TTL = 3600 // 1 hour
 
 const template = {
     name: "blockAppearance",
@@ -74,50 +69,13 @@ type Block = {
     [key: string]: unknown
 }
 
-export async function getBlocks(context?: Context): Promise<Block[]> {
-    // Try to get from Netlify Blobs store first
-    if (context) {
-        try {
-            const store = getStore("cms-cache")
-            const cached = await store.get(CACHE_KEY, { type: "json" })
-            if (cached) {
-                console.log("returning blocks from Netlify Blobs")
-                return cached as Block[]
-            }
-        } catch (error) {
-            console.log(
-                "Cache read failed, proceeding with fresh processing",
-                error
-            )
-        }
-    }
-
-    console.log("processing blocks for the first time")
-
-    // Convert blocks object to array of block objects
+export async function getBlocks(): Promise<Block[]> {
     const blocksArray = Object.values(blocks) as Block[]
 
-    // Add template to the beginning of each block's fields array (spread is faster than unshift)
     const processedBlocks = blocksArray.map((block) => ({
         ...block,
         fields: [template, ...block.fields],
     }))
-
-    // Cache in Netlify Blobs store
-    if (context) {
-        try {
-            const store = getStore("cms-cache")
-            await store.setJSON(CACHE_KEY, processedBlocks, {
-                metadata: {
-                    createdAt: Date.now(),
-                    ttl: CACHE_TTL,
-                },
-            })
-            console.log("cached blocks in Netlify Blobs")
-        } catch (error) {
-            console.log("Cache write failed", error)
-        }
-    }
 
     return processedBlocks
 }
